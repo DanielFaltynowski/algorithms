0928a433ce2f242f6790226524834b7a15853e6d
Daniel Faltynowski <danielfaltynowski@MacBook-Pro-Daniel.local>
2025-06-09 17:44:46 +0200

    Reducers

--- src/reducers.py
+++ src/reducers.py
@@ -1,27 +1,27 @@
-def reduce(arr, acc, reducer):
-    for elem in arr:
-        acc = reducer(acc, elem)
-    return acc
+def reduce(array, accumulator, predicate):
+    for element in array:
+        accumulator = predicate(accumulator, element)
+    return accumulator
 
-def filter(arr, condiction):
-    res = []
-    for elem in arr:
-        if condiction(elem):
-            res.append(elem)
-    return res
+def filter(array, predicate):
+    response = []
+    for element in array:
+        if predicate(element):
+            response.append(element)
+    return response
 
 
-def mapper(arr, scale):
-    res = []
-    for elem in arr:
-        res.append(scale(elem))
-    return  res
+def mapper(array, predicate):
+    response = []
+    for element in array:
+        response.append(predicate(element))
+    return response
 
 
 if __name__ == '__main__':
     l = [1, 2, 3]
 
-    lr = reduce(l, [], lambda acc, elem: acc + [elem * 3])
+    lr = reduce(l, 0, lambda acc, elem: acc + elem)
     lf = filter(l, lambda elem: elem >= 2)
     lm = mapper(l, lambda elem: elem * 4)
 
